6114d1a0fb061042eeb55ba06950aca77ed78daf
Tang Xianghao <t-x.h@163.com>
2021-03-08 22:21:43 +0800

    Format files

--- src/main.ts
+++ src/main.ts
@@ -1,30 +1,25 @@
 import { app, BrowserWindow } from 'electron'
 
-function createWindow()
-{
+function createWindow() {
     const win = new BrowserWindow(
-    {
-        webPreferences:
         {
-            nodeIntegration: true
-        }
-    })
+            webPreferences:
+            {
+                nodeIntegration: true
+            }
+        })
 }
 
 app.whenReady().then(createWindow)
 
-app.on('window-all-closed', () =>
-{
-    if (process.platform !== 'darwin')
-    {
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
         app.quit()
     }
 })
 
-app.on('activate', () =>
-{
-    if (BrowserWindow.getAllWindows().length === 0)
-    {
+app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
         createWindow()
     }
 })
