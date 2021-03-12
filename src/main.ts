fc3fd4f9396fbca29dba362f42a74c2fe59a12fa
Tang Xianghao <t-x.h@163.com>
2021-03-12 22:59:59 +0800

    Setup proxy support

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,39 @@
 import { app, BrowserWindow } from 'electron'
+import fsPromises from 'fs/promises'
 
-function createWindow() {
-    const win = new BrowserWindow({
+import { Configuration } from './config'
+
+const resourceUrls = {
+    javlibrary: 'http://www.javlibrary.com/cn',
+}
+
+function showUrl(window: BrowserWindow, url: string) : Promise<void> {
+    window.title = url
+    return window.loadURL(url)
+}
+
+async function createWindow(config: Configuration) : Promise<void> {
+    const window = new BrowserWindow({
         webPreferences: {
             nodeIntegration: true
         }
     })
-    win.loadURL('https://bing.com')
+
+    if (config.pacPath !== undefined) {
+        // file protocol is not supported by setProxy
+        const pacContent: string = await fsPromises.readFile(config.pacPath, 'utf-8')
+        const encodedPacContent: string = 'data:text/plain;base64,' + Buffer.from(pacContent, 'utf8').toString('base64')
+        await window.webContents.session.setProxy({pacScript: encodedPacContent})
+    }
+
+    await showUrl(window, resourceUrls.javlibrary)
 }
 
-function initApp() {
-    createWindow()
+const config = new Configuration()
+
+async function initApp() : Promise<void> {
+    await config.load(Configuration.getDefaultConfigPath())
+    await createWindow(config)
 }
 
 app.whenReady().then(initApp)
@@ -21,8 +44,8 @@ app.on('window-all-closed', () => {
     }
 })
 
-app.on('activate', () => {
+app.on('activate', async () => {
     if (BrowserWindow.getAllWindows().length === 0) {
-        createWindow()
+        await createWindow(config)
     }
 })
